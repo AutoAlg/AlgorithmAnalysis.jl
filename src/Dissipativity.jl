61f7e18b3e3280ec78ef54f002d7ee7e96c35fc2
Bryan Van Scoy <32279411+vanscoy@users.noreply.github.com>
2023-11-08 17:15:28 -0500

    getting started

--- src/Dissipativity.jl
+++ src/Dissipativity.jl
@@ -1,5 +1,18 @@
+# cd("C:\\Users\\vanscob\\.julia\\dev\\Dissipativity\\")
+# ] activate .
+# using Revise
+# using Dissipativity
+
+# ] test Dissipativity
+
 module Dissipativity
 
 # Write your package code here.
+
+function greet()
+  return "Dissipativity!"
+end
+
+export greet
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "Dissipativity.jl" begin
     # Write your tests here.
+    @test Dissipativity.greet() == "Dissipativity!"
 end
