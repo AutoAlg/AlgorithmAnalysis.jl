7a47f0894109447c5fcd085f2a05fb7b13b4ebba
Bryan Van Scoy <32279411+vanscoy@users.noreply.github.com>
2024-09-25 17:30:13 -0400

    macro with for loops
    The @algorithm macro can now iterate and label for loops.

--- src/startup.jl
+++ src/startup.jl
@@ -1,13 +1,14 @@
 
-cd("C:\\Users\\vanscob\\github\\BlackBoxOptimization.jl\\")
+cd("your-path-here\\AlgorithmAnalysis.jl\\")
 using Pkg
 Pkg.activate(".")
 using Revise
-using BlackBoxOptimization
+using AlgorithmAnalysis
 using Logging
 
 m,L = 1,10
 α = 2/(L+m)
+n = 3
 
 global_logger(ConsoleLogger(stderr, Logging.Info))
 
@@ -20,17 +21,24 @@ global_logger(ConsoleLogger(stderr, Logging.Info))
     # objective function
     f = DifferentiableFunctional{Rⁿ}()
     xs = first_order_stationary_point(f)
-    f' ∈ SectorBounded(m, L, xs, gs)
+    f' ∈ SectorBounded(m, L, xs, f'(xs))
 
-    # algorithm
-    x0 = Rⁿ()
-    x1 = x0 - α*f'(x0)
+    # iterates
+    x = Vector{Rⁿ}(undef, n+1)
+
+    # initial condition
+    x[1] = Rⁿ()
 
     # constraint on initial condition
-    (x0-xs)^2 ≤ 1
+    (x[1]-xs)^2 ≤ 1
+
+    # algorithm
+    for k = 1:n
+        x[k+1] = x[k] - α*f'(x[k])
+    end
 
     # performance measure
-    performance = (x1-xs)^2
+    performance = (x[end]-xs)^2
 end
 
 maximize(performance)
