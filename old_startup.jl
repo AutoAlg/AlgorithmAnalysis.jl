4f5c598550304d9d1cb99c183b4281ae5b718992
Sam Skinner <sebs@Sams-MacBook-Pro-3.local>
2026-04-19 15:52:58 -0400

    sync

--- old_startup.jl
+++ old_startup.jl
@@ -1,7 +1,130 @@
+# using Pkg
+# Pkg.activate(".")
+# using Revise
+using AlgorithmAnalysis
+import Base.:+, Base.:*, Base.:-
+
+# m = 1
+# L = 10
+# α = 2/(L+m)
+
+# @algorithm begin
+#     f = SmoothStronglyConvexFunction{Rⁿ}(m, L)
+#     xs = first_order_stationary_point(f)
+#     fs = f(xs)
+    
+#     x0 = Rⁿ()
+#     g0 = f'(x0)
+#     x1 = x0 - α * g0
+#     x0 => x1
+
+#     performance = (x0 - xs)^2
+# end
+
+# objs = connected_components(performance)
+
+# function smoothstronglyconvexinterpolation(objs::Objects)
+    
+#     objs = deepcopy(objs)
+
+#     fs = filter(x -> x isa SmoothStronglyConvexFunction, objs)
+
+#     for f ∈ fs
+#         m = f.strong_convexity
+#         L = f.smoothness
+
+#         for (xᵢ,fᵢ,gᵢ) ∈ triplets(f), (xⱼ,fⱼ,gⱼ) ∈ triplets(f)
+#             fᵢ-fⱼ ≥ gⱼ'*(xᵢ-xⱼ) + 1/2L*(gᵢ-gⱼ)^2 + m/(2*(1-m/L))*(xᵢ-xⱼ-1/L*(gᵢ-gⱼ))^2
+#         end
+#     end
+    
+#     objs = connected_components(objs)
+
+#     for f ∈ fs
+#         setdiff!(objs, Set([f, f']))
+#     end
+
+#     # need to remove all connections to f and f', not just remove them from the set
+#     for obj ∈ objs
+#         setdiff!(oracles(obj), Set([f, f']))
+#     end
+
+#     objs
+# end
+
+# new_objs = smoothstronglyconvexinterpolation(objs)
+
+# function gramtransformation(objs::Objects)
+    
+#     objs = deepcopy(objs)
+
+#     Xs = Set(filter(T -> T <: InnerProductSpace, typeof.(objs)))
+
+#     for X ∈ Xs
+
+#         xs = filter(x -> x isa X, objs)
+
+#         union!(objs, Gram(xs...))
+
+#         setdiff!(objs, Set(xs))
+#     end
+    
+#     objs
+# end
+
+# struct Transformation
+#     name::String
+#     func::Function
+# end
+
+# transformations = Set{Transformation}([
+#     Transformation(
+#         "Smooth strongly convex interpolation",
+#         smoothstronglyconvexinterpolation
+#     ),
+#     Transformation(
+#         "Gram transformation",
+#         gramtransformation
+#     ),
+# ])
+
+# function interpolation_search(xs::Objects)
+#     visited = Set()
+#     queue = Objects[xs]  # use a deque from DataStructures.jl for better performance
+#     came_from = Dict()
+#     while !isempty(queue)
+#         node = popfirst!(queue)
+#         if verbose
+#             @info "Visiting $node"
+#         end
+
+#         if isimplementable(node)
+#             path = [node]
+#             while node != xs
+#                 node = came_from[node]
+#                 pushfirst!(path, node)
+#             end
+#             return path
+#         end
+#         if node ∉ visited
+#             push!(visited, node)
+#             for neighbor ∈ neighbors(node)
+#                 if neighbor ∉ visited
+#                     if verbose
+#                         @info " ⋅ Queueing $neighbor"
+#                     end
+#                     push!(queue, neighbor)
+#                     came_from[neighbor] = node
+#                 end
+#             end
+#         end
+#     end
+#     error("Path not found")
+# end
+
 using Base.ScopedValues
 
 abstract type NewExpression end
-
 abstract type NewVariable   <: NewExpression end
 abstract type NewConstraint <: NewExpression end
 abstract type NewOracle     <: NewExpression end
@@ -25,13 +148,12 @@ end
 const ALGORITHM_CONTEXT = ScopedValue{AlgorithmState}()
 get_algorithm_context()::AlgorithmState = ALGORITHM_CONTEXT[]
 
+# Centralized registration to remove boilerplate from struct constructors
 function register!(expr::NewExpression, ctx::AlgorithmState = get_algorithm_context())
     if haskey(ctx._expressions, expr.id)
         error("Cannot doubly insert $(expr.id)")
     end
-
     ctx._expressions[expr.id] = expr
-    
     return expr
 end
 
@@ -114,8 +236,6 @@ end
 -(l::ConcretelyValuedVariable, r::LinearDecomposition)      = LinearDecomposition(_merge_terms(_to_dict(l), r.terms, -1.0))
 -(l::LinearDecomposition,      r::ConcretelyValuedVariable) = LinearDecomposition(_merge_terms(l.terms, _to_dict(r, -1.0)))
 -(l::LinearDecomposition,      r::LinearDecomposition)      = LinearDecomposition(_merge_terms(l.terms, r.terms, -1.0))
-
-
 
 ctx = AlgorithmState();
 
