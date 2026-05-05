68a25e932f8cc091e4887896a06f3f8c0cd41d0c
Sam Skinner <sebs@Sams-MacBook-Pro-3.local>
2026-05-05 15:05:16 -0400

    fixed dead code elimination from being rootless

--- src/new/algorithm_context_pruning.jl
+++ src/new/algorithm_context_pruning.jl
@@ -12,54 +12,63 @@ function compute_forward_edges(algorithm_context::AlgorithmContext)::Dict{Expres
     return forward_edges
 end
 
-function _walk_dependencies!(expression_identifier::ExpressionID, algorithm_context::AlgorithmContext, reachable_expressions::Set{ExpressionID})::Nothing
-    if expression_identifier in reachable_expressions
+function _walk_dependencies!(expression_identifier::ExpressionID, algorithm_context::AlgorithmContext, reachable_node_identifiers::Set{UInt32})::Nothing
+    node_identifier::UInt32 = expression_identifier._node_id
+    
+    if node_identifier in reachable_node_identifiers
         return nothing
     end
     
-    push!(reachable_expressions, expression_identifier)
+    push!(reachable_node_identifiers, node_identifier)
     
     for dependency_identifier in dependencies(algorithm_context._expressions[expression_identifier])
-        _walk_dependencies!(dependency_identifier, algorithm_context, reachable_expressions)
+        _walk_dependencies!(dependency_identifier, algorithm_context, reachable_node_identifiers)
     end
     
     return nothing
 end
 
-function compute_reachable_expressions(algorithm_context::AlgorithmContext)::Set{ExpressionID}
-    reachable_expressions::Set{ExpressionID} = Set{ExpressionID}()
+function compute_reachable_node_identifiers(algorithm_context::AlgorithmContext, root_expressions::Vector{ExpressionID})::Set{UInt32}
+    reachable_node_identifiers::Set{UInt32} = Set{UInt32}()
     
-    for (expression_identifier, abstract_expression) in algorithm_context._expressions
-        is_explicitly_named::Bool = haskey(algorithm_context._expression_aliases, expression_identifier) # TODO: this works, but I'm not sure it's entierly correct
-        is_terminal_transition::Bool = abstract_expression isa StateTransition # TODO: make this more explicit
-        
-        if is_explicitly_named || is_terminal_transition
-            _walk_dependencies!(expression_identifier, algorithm_context, reachable_expressions)
+    for root_identifier in root_expressions
+        if !haskey(algorithm_context._expressions, root_identifier)
+            error("provided root expression is not tracked within the given algorithm context")
         end
+        
+        _walk_dependencies!(root_identifier, algorithm_context, reachable_node_identifiers)
     end
     
-    return reachable_expressions
+    return reachable_node_identifiers
 end
 
-function eliminate_unreachable_expressions!(algorithm_context::AlgorithmContext)::Nothing
+function extract_reachable_subgraph!(algorithm_context::AlgorithmContext, reachable_node_identifiers::Set{UInt32})::Nothing
     if try_get_algorithm_context() !== nothing
-        error("Compiler passes must be evaluated without an active ALGORITHM_CONTEXT.")
+        error("compiler passes must be evaluated without an active context")
     end
 
-    reachable_expressions::Set{ExpressionID} = compute_reachable_expressions(algorithm_context)
     all_tracked_identifiers::Vector{ExpressionID} = collect(keys(algorithm_context._expressions))
     
     for expression_identifier in all_tracked_identifiers
-        if !(expression_identifier in reachable_expressions)
+        if !(expression_identifier._node_id in reachable_node_identifiers)
             delete!(algorithm_context._expressions, expression_identifier)
+            delete!(algorithm_context._expression_aliases, expression_identifier)
         end
     end
     
     return nothing
 end
 
-function eliminate_unreachable_expressions(source_context::AlgorithmContext)::AlgorithmContext
+function eliminate_unreachable_expressions(source_context::AlgorithmContext, root_expressions::Vector{ExpressionID})::AlgorithmContext
+    reachable_node_identifiers::Set{UInt32} = compute_reachable_node_identifiers(source_context, root_expressions)
+    
     cloned_context::AlgorithmContext = clone(source_context)
-    eliminate_unreachable_expressions!(cloned_context)
+    extract_reachable_subgraph!(cloned_context, reachable_node_identifiers)
+    
     return cloned_context
+end
+
+function eliminate_unreachable_expressions(source_context::AlgorithmContext, root_expressions::Vector{<:NewExpression})::AlgorithmContext
+    root_identifiers::Vector{ExpressionID} = ExpressionID[abstract_expression.id for abstract_expression in root_expressions]
+    return eliminate_unreachable_expressions(source_context, root_identifiers)
 end
--- startup.jl
+++ startup.jl
@@ -8,7 +8,7 @@ using Base.ScopedValues
 original_context::AlgorithmContext = AlgorithmContext()
 step_size::Float64 = 0.05
 
-with_context(original_context) do
+t = with_context(original_context) do
     f, ∇f = SSC(3, 10)
     set_alias!(f, "f")
     set_alias!(∇f, "∇f")
@@ -21,10 +21,12 @@ with_context(original_context) do
     
     transition = (x0 => x1)
     set_alias!(transition, "step")
+
+    transition
 end
 
 print(original_context)
 
-pruned_context::AlgorithmContext = eliminate_unreachable_expressions(original_context)
+pruned_context::AlgorithmContext = eliminate_unreachable_expressions(original_context, [t])
 
 print(pruned_context)
